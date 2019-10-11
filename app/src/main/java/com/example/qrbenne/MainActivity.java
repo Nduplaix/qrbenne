ecc4263bd1483bed7da0a3f55b105077e644ddfb
matthieu <matthieubeaussart8@gmail.com>
2019-10-11 15:02:29 +0200

    Feat SMS Handler

--- app/src/main/java/com/example/qrbenne/MainActivity.java
+++ app/src/main/java/com/example/qrbenne/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SmsActivity mSmsActivity = new SmsActivity(this, this);
+        // TODO Envoyer le SMS quelque part
     }
 }
