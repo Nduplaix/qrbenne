a416563e79905418ccd1502f2ebd12a457b28f3c
matthieu <matthieubeaussart8@gmail.com>
2019-10-11 22:22:46 +0200

    FEAT OSM Map - Add OSM Activity

---
+++ app/src/main/java/com/example/qrbenne/OSMActivity.java
@@ -0,0 +1,14 @@
+package com.example.qrbenne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OSMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_osm);
+    }
+}
